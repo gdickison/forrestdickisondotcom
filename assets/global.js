525185744e63d0672e1a8bf9ef86d96110d62963
paunchymonk <gregory@dickison.net>
2021-12-21 08:56:05 -0800

    Modified some things

--- assets/global.js
+++ assets/global.js
@@ -728,3 +728,51 @@ class VariantRadios extends VariantSelects {
 }
 
 customElements.define('variant-radios', VariantRadios);
+
+function responsiveSlider(){
+    console.log("Running");
+    var slider = document.getElementById("hero-slider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("hero-slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    // var prev = document.getElementById("prev");
+    // var next = document.getElementById("next");
+
+    window.addEventListener('resize', function() {
+      sliderWidth = slider.offsetWidth;
+    });
+
+    var nextSlide = function() {
+      if(count < items) {
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = items) {
+        slideList.style.left = "0px";
+        count = 1;
+      }
+    };
+
+    var prevSlide = function() {
+      if(count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = 1) {
+        count = items - 1;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+    };
+
+    setInterval(function() {
+      nextSlide()
+    }, 5000);
+
+};
+
+window.onload = function() {
+    responsiveSlider();
+}
